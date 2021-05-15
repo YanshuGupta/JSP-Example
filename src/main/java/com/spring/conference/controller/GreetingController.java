be1f39dadec9bd38113d2e600c571c17835e0932
shubhamkgupta14 <skshubham0786@gmail.com>
2021-05-15 12:00:38 +0530

    First

---
+++ src/main/java/com/spring/conference/controller/GreetingController.java
@@ -0,0 +1,16 @@
+package com.spring.conference.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GreetingController {
+	
+	@GetMapping("greeting")
+	public String greet(Map<String, Object> model) {
+		model.put("message", "HELLO BRYAN");
+		return "greeting";
+	}
+}
